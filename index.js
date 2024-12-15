2ea5b6b2107b45ccd60e403b37ac5ba4a0807110
Harsh Patel <harshpatelh26@gmail.com>
2024-12-15 22:39:32 +0530

    express C10

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const http = require("http");
-const PORT = 8001;
-const fs= require("fs");
-const myServer = http.createServer((req,res)=>{
-    if(req.url=== '/favicon.ico')return res.end();
-    const log = `${Date.now()} ${req.method} ${req.url}: new req recieved \n`;
-    fs.appendFile("log.txt",log,(err,data)=>{
-        res.end("Hello from Server")
-    })
-    
-},)
+const express = require('express');
 
-myServer.listen(PORT,()=>{
-    console.log(`Server is connected on PORT no : ${PORT}`)});
+// const fs= require("fs");
+const app = express();
+const PORT = 8001;
+
+app.get('/',(req,res)=>{
+    return res.send("Home Page");
+});
+
+app.get('/about',(req,res)=>{
+    return res.send("About Page" + " Hey " + req.query.name + ' You are at ' + req.query.age + ' years old');
+})   
+
+app.listen(PORT,()=>{
+         console.log(`Server is connected on PORT no : ${PORT}`)})
+
+// const myServer = http.createServer(app);
+// const myServer = http.createServer((req,res)=>{
+//     if(req.url=== '/favicon.ico')return res.end();
+//     const log = `${Date.now()} ${req.method} ${req.url}: new req recieved \n`;
+//     fs.appendFile("log.txt",log,(err,data)=>{
+//         res.end("Hello from Server")
+//     })
+    
+// },)
+
+// myServer.listen(PORT,()=>{
+//     console.log(`Server is connected on PORT no : ${PORT}`)});
     
